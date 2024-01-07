7ee66181eca5e5c15a8dcc1a320f98830c01b392
Dhruv Yadav <dhruv.yadav@vanderbilt.edu>
2024-01-07 17:45:17 -0500

    adding team name

--- epl/main.py
+++ epl/main.py
@@ -17,10 +17,11 @@ def create_table_all_player_all_stats():
     if response.status_code != 200:
         raise Exception("Response was code " + str(response.status_code))
     
-    data = sorted(json.loads(response.text)["elements"], key=lambda x: x["id"]) ### this is a list
-    json_object = json.dumps(data, indent=4) ### this is a string
-    print(data[354])
-    columns=["web_name", "form", "now_cost", "selected_by_percent", "event_points", "total_points", "ict_index", "influence", "creativity", "threat", "transfers_in", "transfers_out", "bonus"]
+    all_data = json.loads(response.text) ### this is a list
+    player_data = sorted(all_data["elements"], key=lambda x: x["id"])
+    team_data=all_data["teams"]
+
+    columns=["web_name", "team", "form", "now_cost", "selected_by_percent", "event_points", "total_points", "ict_index", "influence", "creativity", "threat", "transfers_in", "transfers_out", "bonus"]
     
     with open("players_stats.csv", 'w', newline='') as csv_file:
         csv_writer = csv.writer(csv_file)
@@ -29,7 +30,19 @@ def create_table_all_player_all_stats():
         
     with open("players_stats.csv", "a", newline='', encoding="utf-8") as csv_file:
         csv_writer = csv.writer(csv_file)
-        for json_data in data:
+        for json_data in player_data:
             values = [json_data.get(c, '') for c in columns]
+            values[1]=[team["name"] for team in team_data if team.get("id") == values[1]][0]
             csv_writer.writerow(values)
+
+
+    all_data = json.loads(response.text)
+
+    all_json_objects=json.dumps(all_data, indent=4)
+
+    with open("sample.json", 'w') as json_file:
+        json_file.write(all_json_objects)
+
+
+create_table_all_player_all_stats()
 
